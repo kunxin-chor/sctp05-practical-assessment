025360507d6249f854e5811c1355f2636bde61dd
kunxin-chor <chorkunxin@yahoo.com>
2025-01-08 06:59:51 +0000

    Added state to manage the mobile navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ProductCard from './ProductCard'
 
 export default function App() {
+
+  const [isNavBarShowing, setIsNavBarShowing] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsNavBarShowing(!isNavBarShowing);
+  }
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -10,15 +17,14 @@ export default function App() {
           <button
             className="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
             aria-controls="navbarNav"
             aria-expanded="false"
             aria-label="Toggle navigation"
+            onClick={toggleNavbar}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className={`collapse navbar-collapse ${isNavBarShowing ? "show" : ""}`} id="navbarNav">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
