3f38df8e390b5e7c983efba9c8a853d4392f7cd6
kunxin-chor <chorkunxin@yahoo.com>
2025-01-17 06:34:25 +0000

    Added modify and delete cart item

--- src/CartStore.js
+++ src/CartStore.js
@@ -21,8 +21,10 @@ export const cartAtom = atom(initialCart);
 
 // custom hook
 // a custom hook is a way to share functions between components
+// we often create functions that perform CRUD on the jotai atom
+// and then share them via custom hooks
 export const useCart = () => {
-    const [cart, setCart] = useAtom(cartAtom);
+    const [cart, setCart] = useAtom(cartAtom); // -> one atom is one shared data in Jotai
 
     const getCartTotal = () => {
         let total = 0;
@@ -35,35 +37,60 @@ export const useCart = () => {
     }
 
     const addToCart = (product) => {
-        console.log(product)
         setCart(currentCart => {
-
             // find if the item already exists in the shopping item
             const existingItemIndex = cart.findIndex(i => i.product_id === product.product_id);
-            console.log(existingItemIndex);
             if (existingItemIndex !== -1) {
-
                 let newQuantity = cart[existingItemIndex].quantity + 1;
-                console.log("new quantity", newQuantity);
+
                 // existing item
-                const modifiedCart= currentCart.setIn([existingItemIndex, 'quantity'], newQuantity);
+                const modifiedCart = currentCart.setIn([existingItemIndex, 'quantity'], newQuantity);
                 return modifiedCart;
             } else {
-
                 // new item
                 return currentCart.concat({
                     ...product,
                     quantity: 1
                 })
+            }
+        })
+    }
 
+    const modifyQuantity = (product_id, quantity) => {
+        // updating the atom in Jotai is asynchronous
+        setCart(currentCart => {
 
+            // 1. find the index of the cart item for the product_id
+            const existingItemIndex = currentCart.findIndex(i => i.product_id === product_id);
+
+            // 2. If the new quantity is more than 0, then we'll just update the new quantity
+            if (quantity > 0) {
+                // .setIn will return a modified copy of the original array
+                const modifiedCart = currentCart.setIn([existingItemIndex, "quantity"], quantity);
+                return modifiedCart;
+            } else {
+                // 3. If the new quantity is 0
+                // const lhs = currentCart.slice(0,existingItemIndex-1);
+                // const rhs = currentCart.slice(existingItemIndex+1);
+                // return [...lhs, ...rhs];
+                return currentCart.filter(cartItem => cartItem.product_id != product_id);
             }
 
+
+
+        })
+
+
+    }
+
+    const removeFromCart = (product_id) => {
+        setCart(currentCart => {
+            return currentCart.filter(cartItem => cartItem.product_id != product_id);
         })
     }
 
     return {
-        cart, getCartTotal, addToCart
+        cart, getCartTotal, addToCart, modifyQuantity, removeFromCart
     }
 
 }
--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useCart } from './CartStore';
 
 export default function ShoppingCart() {
-    const {cart, getCartTotal} = useCart();
+    const {cart, getCartTotal, modifyQuantity, removeFromCart} = useCart();
     return <div className="container mt-4">
         <h2>Shopping Cart</h2>
         {
@@ -16,7 +16,23 @@ export default function ShoppingCart() {
                                         <img src={item.imageUrl}/>
                                         <div>
                                             <h5>{item.productName}</h5>
-                                            <p>Quantity: {item.quantity}</p>
+                                            <div className="d-flex align-items-center">
+                                                <button className="btn btn-sm btn-secondary me-2" onClick={()=>{
+                                                    modifyQuantity(item.product_id, item.quantity+1);
+                                                }}>+</button>
+
+                                                <p className="mb-0">Quantity: {item.quantity}</p>
+                                            
+                                                <button className="btn btn-sm btn-secondary ms-2" onClick={()=>{
+                                                    modifyQuantity(item.product_id, item.quantity-1);
+                                                }}>-</button>
+
+                                                <button className="btn btn-sm btn-danger ms-2" onClick={()=>{
+                                                    removeFromCart(item.product_id)
+                                                }}>Delete</button>
+
+                                            </div>
+                                           
                                             <p>Per Price: ${item.price}</p>
                                         </div>
                                         <span>${(item.price * item.quantity).toFixed(2)}</span>
